79e813db3f429d9276a7ebd5b528c6bed69109f8
Jiheng Nie <Niejiheng@gmail.com>
2023-01-25 19:32:24 +0000

    Completed ps2 and started on ps3

--- 60001/psets/p1/ps1b.py
+++ 60001/psets/p1/ps1b.py
@@ -2,20 +2,24 @@
 annual_salary = float(input("Enter your annual salary: "))
 portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
 total_cost = float(input("Enter the cost of your dream home: "))
+semi_annual_raise = float(input("Enter as a decimal percentage: "))
 
 # state variables
 portion_down_payment = .25 * total_cost
 r = .04
 months = 0
-
-# Current saving variables
 current_savings = 0.0
-salary_saved_per_month = annual_salary * portion_saved / 12
 
 # algorithm
 while current_savings < portion_down_payment:
+  if months >= 6 and months % 6 == 0 :
+    annual_salary = annual_salary + annual_salary * semi_annual_raise
+
   annual_return_from_savings = current_savings * r / 12
+  salary_saved_per_month = annual_salary * portion_saved / 12
+
   current_savings = current_savings + annual_return_from_savings + salary_saved_per_month
   months += 1
-  #print(current_savings, months)
+  # print(current_savings, months, annual_salary)
+
 print(months)
---
+++ 60001/psets/p1/ps1c.py
@@ -0,0 +1,14 @@
+# inputs
+annual_salary = float(input("Enter your annual salary: "))
+
+# state variables
+semi_annual_raise = .07
+r = .04
+down_payment = .25
+total_cost = 1000000
+current_savings = 0.0
+best_rate = 10000
+worst_rate = 0
+steps = 0
+
+# algorithm
