4782a4ff80d28c62a320c2e20a6ca81f5fdae1ca
Jiheng Nie <Niejiheng@gmail.com>
2023-01-31 01:12:09 +0000

    Finished lecture 4

---
+++ 60001/lectures/lecture4/example.py
@@ -0,0 +1,16 @@
+def is_even_without_return(i):
+  """
+  Input: i, a positive int
+  Does not return anything
+  """
+  print("Without return")
+  remainder = i % 2
+
+
+def is_even_with_return(i):
+  """
+  Input: i, a positive int
+  Returns true if i is even, false if i is odd
+  """
+  print("Without return")
+  return  i % 2 == 0
